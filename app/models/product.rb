c3f0cda27b926c5f8dfbea118d3c76ed27cb2816
Kyle Dornblaser <kdornblaser@gmail.com>
2015-01-21 12:55:00 -0500

    Modified product has many through assocation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   has_many :releases
+  has_many :launch_dates, through: :releases
+  has_many :regions, through: :releases
+  has_many :mediums, through: :releases
 end
