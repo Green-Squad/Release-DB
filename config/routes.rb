25618a1a52bd621d2e7e7d6334453fc28fc2f030
Kyle Dornblaser <kdornblaser@gmail.com>
2015-01-23 09:51:00 -0500

    Add home page controller, views, tests, and assets.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'home#index'
+
   resources :categories, only: [:index, :show]
   resources :products
+  
+  # Admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'welcome#index'
+
 
 end
