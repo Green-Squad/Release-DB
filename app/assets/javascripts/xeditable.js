a5e77e6bdea799831340489737d942eb7923e972
Kyle Dornblaser <kdornblaser@gmail.com>
2015-01-27 11:44:41 -0500

    Add working new release button

--- app/assets/javascripts/xeditable.js
+++ app/assets/javascripts/xeditable.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
   $.fn.editable.defaults.mode = 'inline';
+  $.fn.editable.defaults.onblur = 'ignore';
   $.fn.editable.defaults.ajaxOptions = {
     type : "put"
   };
 
   //init editables
   $('.editable').editable('disable');
-  $('.new-release-editable').editable();
   $('.new-release-entry').hide();
 
   $('#edit-button').click(function(e) {
     $('.editable').editable('toggleDisabled');
-    //$('.editable').editable('show', false);
+    $('.editable').editable('show', false);
     
     $('.new-release-entry').toggle();
     $('.new-release-editable').editable('toggleDisabled');
-    //$('.new-release-editable').editable('show', false);
+    $('.new-release-editable').editable('show', false);
+    
+    
+    $('#add-release-button').toggle();
+
     
     e.stopPropagation();
   });
+  
 
   $('.editable').last().on('shown', function(e, editable) {
     if (arguments.length == 2) {
@@ -28,7 +33,72 @@ $(document).ready(function() {
     }
   });
 
-  $('#new-release-save').click(function() {
+
+  $('#add-release-button').click(function() {
+    if ($('.new-release-entry').empty()) {
+      $('.new-release-entry').append(
+          "<div>" +
+          "<span class='new-release-editable' " + 
+          "id='region_id' " + 
+          "data-type='select' " + 
+          "data-pk='' " +
+          "data-source='/regions.json' " + 
+          "data-showbuttons='false' " + 
+          "data-send='auto'>" +
+          "</span>" +
+          " / " +
+          "<span class='new-release-editable' " + 
+          "id='launch_date' " +  
+          "data-type='text' " +  
+          "data-pk='' " +  
+          "data-placeholder='Release Date' " +
+          "data-send='auto'>" + 
+          "</span>" +
+          " / " +
+          "<span class='new-release-editable' " +  
+          "id='medium_id' " + 
+          "data-type='select' " +  
+          "data-pk='' " +  
+          "data-source='/media.json?category_id=" + 
+          $('.temp_information').data('category-id') +
+          "' " +  
+          "data-showbuttons='false' " +  
+          "data-send='auto'>" + 
+          "</span>" + 
+          "<div style='display:none'>" + 
+          "<span class='new-release-editable' " +  
+          "id='product_id' " + 
+          "data-value='" +
+          $('.temp_information').data('product-id') +
+          "' " +  
+          "data-pk='' " +  
+          "data-showbuttons='false' " +  
+          "data-send='auto' " + 
+          "style='display:none'>" + 
+          "</span>" +
+          "</div>" +
+          "<button id='new-release-save'>" + 
+          "Save" +
+          "</button>" +
+          "</div>"
+          )
+          
+          $('.new-release-editable').editable();
+          $('.new-release-editable').editable('show', false);
+          
+          bindClick();
+      }
+  });
+  
+  $('.editable').on('save', function(e, params) {
+    var msg = 'Your contribution has been submitted for approval.';
+    $('#msg').addClass('alert-success').addClass('alert').removeClass('alert-danger').html(msg).show();
+});
+
+  bindClick = function() {
+    
+    $('#new-release-save').click(function() {
+    console.log('clicked')
     $('.new-release-editable').editable('submit', {
       url : '/releases',
       ajaxOptions : {
@@ -46,12 +116,18 @@ $(document).ready(function() {
           //remove unsaved class
           $(this).removeClass('editable-unsaved');
 
+          $(this).removeClass('new-release-editable');
+          $(this).addClass('editable');
           //show messages
-          var msg = 'New release created!';
-          $('#msg').addClass('alert-success').removeClass('alert-error').html(msg).show();
+          var msg = 'Your contribution has been submitted for approval.';
+          $('#msg').addClass('alert-success').addClass('alert').removeClass('alert-danger').html(msg).show();
 
           //hide new release button
-          $('#new-release-save').hide();
+          $('#new-release-save').remove();
+          
+          $('.new-release-entry').removeClass('new-release-entry');
+          $('#release-list').append('<div class="new-release-entry"></div>');
+          
         } else if (data && data.errors) {
           //server-side validation error, response like {"errors": {"username": "username already exist"} }
           config.error.call(this, data.errors);
@@ -66,8 +142,9 @@ $(document).ready(function() {
             msg += k + ": " + v + "<br>";
           });
         }
-        $('#msg').removeClass('alert-success').addClass('alert-error').html(msg).show();
+        $('#msg').removeClass('alert-success').addClass('alert').addClass('alert-danger').html(msg).show();
       }
     });
   });
+  };
 });
