3313c39cb5d948f5a3e6541985ec6c740def87d3
Kyle Dornblaser <kdornblaser@gmail.com>
2015-01-29 09:53:14 -0500

    Add live veresion to approval table to compare. Add colors to approval table to group live and pending version. Order versions by created at.

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,6 +1,6 @@
 class ApprovalsController < ApplicationController
   def index
-    @pending_products = PaperTrail::Version.where(item_type: "Product", status: "pending")
+    @pending_products = PaperTrail::Version.where(item_type: "Product", status: "pending").order(:created_at)
     @pending_releases = PaperTrail::Version.where(item_type: "Release", status: "pending")
   end
   
