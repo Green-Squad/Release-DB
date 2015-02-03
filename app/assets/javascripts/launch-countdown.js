f8fb677b68020b67b215788f7c08b1c644e2f22b
Kyle Dornblaser <kdornblaser@gmail.com>
2015-02-03 12:59:43 -0500

    Remove countdownjs (countup) from dates in the past

--- app/assets/javascripts/launch-countdown.js
+++ app/assets/javascripts/launch-countdown.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $('.date').each(function() {
     var _this = $(this)
-    countdown(Date.parse($(this).data('launch-date')), function(ts) {
-      _this.html(ts.toHTML());
-    }, countdown.YEARS | countdown.MONTHS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
-
+    launchDate = Date.parse($(this).data('launch-date'));
+    if(launchDate && launchDate > Date.today()) {
+      countdown(launchDate, function(ts) {
+        _this.html(ts.toHTML());
+      }, countdown.YEARS | countdown.MONTHS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+    }
   });
 }); 
