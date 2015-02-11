04fbedf535b8dafbb2b565ac46ba1fcecdb034e6
Kyle Dornblaser <kdornblaser@gmail.com>
2015-02-11 13:01:34 -0500

    Add books and tv shows to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,7 +52,7 @@ regions.each do |region|
 end
 
 # Add Global to regions
-Region.create(name: "Global")
+global_region = Region.create(name: "Global")
 
 # Metacritic api key and base url
 headers =  {"X-Mashape-Key" => ENV["X_MASHAPE_KEY"]}
@@ -61,7 +61,6 @@ base_url = "https://byroredux-metacritic.p.mashape.com/"
 
 # Import metacritic data for movies and music
 category = Category.where(name: "Games").first
-region   = Region.where(name: "Global").first
 game_platforms = {
                     "ps4"     => Medium.where(name: "PlayStation 4", category: category).first, 
                     "xboxone" => Medium.where(name: "Xbox One", category: category).first,
@@ -92,7 +91,7 @@ list_types.each do |list_type|
       
       product = Product.where(name: name, category: category).first_or_create
       launch_date_record = LaunchDate.where(launch_date: launch_date).first_or_create
-      Release.where(product: product, region: region, launch_date: launch_date_record, medium: medium, source: source).first_or_create
+      Release.where(product: product, region: global_region, launch_date: launch_date_record, medium: medium, source: source).first_or_create
     end
   end
 end
@@ -119,9 +118,59 @@ data.each do |datum|
         
         product = Product.where(name: name, category: datum[:category]).first_or_create
         launch_date_record = LaunchDate.where(launch_date: launch_date).first_or_create
-        Release.where(product: product, region: region, launch_date: launch_date_record, medium: datum[:medium], source: source).first_or_create
+        Release.where(product: product, region: global_region, launch_date: launch_date_record, medium: datum[:medium], source: source).first_or_create
     end
   end
+end
+
+# Books
+book_category = Category.where(name: "Books").first
+
+books = [
+          { name: "Halo: New Blood", launch_date: "2015-03-02", medium: "Ebook", source: "http://books.simonandschuster.com/Halo-New-Blood/Matt-Forbeck/9781476796703"},
+          { name: "Halo: New Blood", launch_date: "2015-03-02", medium: "Audiobook", source: "http://books.simonandschuster.com/Halo-New-Blood/Matt-Forbeck/9781476796703"},
+          { name: "Go Set a Watchman", launch_date: "2015-07-14", medium: "Ebook", source: "http://www.barnesandnoble.com/w/go-set-a-watchman-harper-lee/1121151104"},
+          { name: "Go Set a Watchman", launch_date: "2015-07-14", medium: "Paperback", source: "http://www.barnesandnoble.com/w/go-set-a-watchman-harper-lee/1121151104"},
+          { name: "Go Set a Watchman", launch_date: "2015-07-14", medium: "Hardcover", source: "http://www.barnesandnoble.com/w/go-set-a-watchman-harper-lee/1121151104"},
+          { name: "Go Set a Watchman", launch_date: "2015-07-14", medium: "Audiobook", source: "http://www.barnesandnoble.com/w/go-set-a-watchman-harper-lee/1121151104"},
+          { name: "The Whites", launch_date: "2015-02-15", medium: "Ebook", source: "http://www.barnesandnoble.com/w/the-whites-richard-price/1121153283"},
+          { name: "The Whites", launch_date: "2015-02-15", medium: "Hardcover", source: "http://www.barnesandnoble.com/w/the-whites-richard-price/1121153283"},
+          { name: "The Whites", launch_date: "2015-02-15", medium: "Audiobook", source: "http://www.barnesandnoble.com/w/the-whites-richard-price/1121153283"},
+          { name: "A Touch of Stardust", launch_date: "2015-02-17", medium: "Ebook", source: "http://www.barnesandnoble.com/w/a-touch-of-stardust-kate-alcott/1119641174"},
+          { name: "A Touch of Stardust", launch_date: "2015-02-17", medium: "Hardcover", source: "http://www.barnesandnoble.com/w/a-touch-of-stardust-kate-alcott/1119641174"},
+          { name: "A Touch of Stardust", launch_date: "2015-02-17", medium: "Audiobook", source: "http://www.barnesandnoble.com/w/a-touch-of-stardust-kate-alcott/1119641174"},
+          { name: "Minecraft: Blockopedia", launch_date: "2015-02-24", medium: "Hardcover", source: "http://www.barnesandnoble.com/w/minecraft-scholastic/1120018893"},
+          { name: "The Assassin", launch_date: "2015-03-03", medium: "Ebook", source: "http://www.barnesandnoble.com/w/the-assassin-clive-cussler/1119859169"},
+          { name: "The Assassin", launch_date: "2015-03-03", medium: "Hardcover", source: "http://www.barnesandnoble.com/w/the-assassin-clive-cussler/1119859169"},
+          { name: "The Assassin", launch_date: "2015-03-03", medium: "Audiobook", source: "http://www.barnesandnoble.com/w/the-assassin-clive-cussler/1119859169"},
+          { name: "I Hate Myselfie: A Collection of Essays by Shane Dawson", launch_date: "2015-03-10", medium: "Ebook", source: "http://www.barnesandnoble.com/w/i-hate-myselfie-shane-dawson/1120840897"},
+          { name: "I Hate Myselfie: A Collection of Essays by Shane Dawson", launch_date: "2015-03-10", medium: "Paperback", source: "http://www.barnesandnoble.com/w/i-hate-myselfie-shane-dawson/1120840897"},
+          { name: "Memory Man", launch_date: "2015-04-21", medium: "Ebook", source: "http://www.barnesandnoble.com/w/memory-man-david-baldacci/1120259031"},
+          { name: "Memory Man", launch_date: "2015-04-21", medium: "Paperback", source: "http://www.barnesandnoble.com/w/memory-man-david-baldacci/1120259031"},
+          { name: "Memory Man", launch_date: "2015-04-21", medium: "Hardcover", source: "http://www.barnesandnoble.com/w/memory-man-david-baldacci/1120259031"},
+          { name: "Memory Man", launch_date: "2015-04-21", medium: "Audiobook", source: "http://www.barnesandnoble.com/w/memory-man-david-baldacci/1120259031"}
+        ]
+books.each do |book|
+  product = Product.where(name: book[:name], category: book_category).first_or_create
+  launch_date_record = LaunchDate.where(launch_date: book[:launch_date]).first_or_create
+  medium_record = Medium.where(name: book[:medium]).first
+  Release.where(medium: medium_record, region: global_region, launch_date: launch_date_record, product: product, source: book[:source]).first_or_create
+end
+
+# TV
+tv_category = Category.where(name: "TV").first
+tv_seasons = [
+          { name: "Better Call Saul: Season 1", launch_date: "2015-02-08", medium: "Broadcast", source: "http://www.metacritic.com/tv/better-call-saul"},
+          { name: "The Walking Dead: Season 5", launch_date: "2014-10-12", medium: "Broadcast", source: "http://www.metacritic.com/tv/the-walking-dead/season-5"},
+          { name: "Game of Thrones: Season 5", launch_date: "2015-04-12", medium: "Broadcast", source: "http://en.wikipedia.org/wiki/Game_of_Thrones_(season_5)"},
+          { name: "Archer: Season 6", launch_date: "2015-01-08", medium: "Broadcast", source: "http://en.wikipedia.org/wiki/List_of_Archer_episodes"},
+          { name: "House of Cards: Season 3", launch_date: "2015-02-27", medium: "Netflix", source: "http://www.imdb.com/title/tt1856010/episodes?season=3&ref_=tt_eps_sn_3"}
+        ]
+tv_seasons.each do |season|
+  product = Product.where(name: season[:name], category: tv_category).first_or_create
+  launch_date_record = LaunchDate.where(launch_date: season[:launch_date]).first_or_create
+  medium_record = Medium.where(name: season[:medium]).first
+  Release.where(medium: medium_record, region: global_region, launch_date: launch_date_record, product: product, source: season[:source]).first_or_create
 end
 
 Product.paper_trail_on!
