29dcd588931698cc43f5f2341928953dc28d2210
Jordan Smith <jordan.r.smith@gmail.com>
2015-02-12 11:16:19 -0500

    Update affiliate link. Add digital ocean link to footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def affiliate_link(name, medium, category)
-    "http://www.amazon.com/s/tag=gresqu-20&field-keywords=#{name} #{medium} #{category}"
+    "http://www.amazon.com/s/?tag=gresqu-20&field-keywords=#{name} #{medium} #{category}"
   end
   
   # Returns the full title on a per-page basis.
