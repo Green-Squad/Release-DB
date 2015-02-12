a2f572573844a77bdd57dcb6dcbc6edd3ea36d4b
Jordan Smith <jordan.r.smith@gmail.com>
2015-02-12 14:30:37 -0500

    Update base page title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Release Dates"
+    base_title = "Release DB"
     if page_title.empty?
       base_title
     else
